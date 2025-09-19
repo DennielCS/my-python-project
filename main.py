5cca958f03ef3af3a2004f34b69f3ac268b80ea2
Denniel Marth Enraca <dennielmarth.enraca@benilde.edu.ph>
2025-09-19 15:50:30 +0800

    Exercise ch04 done

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import sys
+
 print("Hello, Git!")
+
+
+
+
+text = '7.2'
+number       = float(text) # value = 7.2
+whole_number = int(number) # value = 7
+print(number, whole_number)
+
+
+print(f"The current version of Python is {sys.version}")
+print(f"The whole number of 7.2 is {whole_number}")
+
+
+# Exercise
+
+given = input("Type any number: ")
+square = int(given)**2
+cube = int(given)**3
+print(f"The square root of {given} is {square}")
+print(f"The cube root of {given} is {cube}")
+
+
+
+
